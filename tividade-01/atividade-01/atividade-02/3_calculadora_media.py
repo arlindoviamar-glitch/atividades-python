87f2ed877a7c2eb1b9654b5bb4e547e07953d728
Tiago Kauan <arlindo.viamar@gmail.com>
2025-09-30 21:15:35 -0300

    Update 3_calculadora_media.py

--- tividade-01/atividade-01/atividade-02/3_calculadora_media.py
+++ tividade-01/atividade-01/atividade-02/3_calculadora_media.py
@@ -1,1 +1,15 @@
 
+# 3- Calculadora de Média Escolar
+nota1 = 7.5
+nota2 = 8.0
+nota3 = 6.5
+
+# Calcula a média das notas
+media = (nota1 + nota2 + nota3) / 3
+
+# Exibe os resultados
+print("--- Média Escolar ---")
+print(f"Nota 1: {nota1}")
+print(f"Nota 2: {nota2}")
+print(f"Nota 3: {nota3}")
+print(f"Média Final: {media:.2f}")
